ca94b8e473479b944a41d8b9ecb35048452a2501
taphead <techne.trax@proton.me>
2025-06-16 10:19:42 +0600

    added description section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Container from "./components/container/Container";
 import Navbar from "./components/navbar/Navbar";
+import DescriptionSection from "./components/description_section/DescriptionSection";
 
 function App() {
   return (
     <>
       <Container>
         <Navbar />
+        <DescriptionSection />
       </Container>
     </>
   );
--- src/components/description_section/DescriptionSection.jsx
+++ src/components/description_section/DescriptionSection.jsx
@@ -1,1 +1,22 @@
 import "./DescriptionSection.css";
+import marketingImage from "../../assets/illustration.png";
+
+export default function DescriptionSection() {
+  return (
+    <section className="description-section">
+      <div className="description-text">
+        <h1>Navigating the digital landscape for success</h1>
+        <p>
+          Our digital marketing agency helps businesses grow and succeed online
+          through a range of services including SEO, PPC, social media
+          marketing, and content creation.
+        </p>
+        <button className="consultation-button">Book a consultation</button>
+      </div>
+
+      <div className="description-image">
+        <img src={marketingImage} alt="Digital Marketing" />
+      </div>
+    </section>
+  );
+}
